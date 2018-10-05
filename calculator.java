ee538b85aeb7f6d01aa90a9e1b1bec67795af35b
Taerim Lee <ltr0121@naver.com>
2018-10-05 14:40:24 +0900

    public 오타 수정

--- calculator.java
+++ calculator.java
@@ -1,9 +1,11 @@
 public class claculator{
-    publid static int sum(int a,int b){
+    public static int sum(int a,int b){
         return a + b;
     }
 
-    publid static int minus(int a,int b){
+    public static int minus(int a,int b){
         return a - b;
     }
+
+
 }
